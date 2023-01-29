630d1356374cc07f4dde601cde7ecee0d03d23fb
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-29 14:13:39 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/andersondev/treinarspringbootmongodb/TreinarSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.andersondev.treinarspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinarSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinarSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersondev/treinarspringbootmongodb/TreinarSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersondev.treinarspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinarSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
