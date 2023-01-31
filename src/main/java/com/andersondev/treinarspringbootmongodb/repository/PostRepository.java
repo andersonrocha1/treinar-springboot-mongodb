3d90622d95386a92a4d1f886da0044321b3be705
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-31 10:22:12 -0300

    Consulta simples com query methods

--- src/main/java/com/andersondev/treinarspringbootmongodb/repository/PostRepository.java
+++ src/main/java/com/andersondev/treinarspringbootmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.andersondev.treinarspringbootmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.andersondev.treinarspringbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
